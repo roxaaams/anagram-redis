83fbc2bb7f4a657112578f9abb2b9311d7538069
Roxana Mafteiu-Scai <roxanamafteiuscai@gmail.com>
2021-09-30 15:12:27 +0200

    feat: create promt UI

---
+++ src/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+import input from 'input';
+import { getKey} from './redis/client';
+
+(async () => {
+  while (true) {
+    const selection = await input.select(
+      'Menu: ',
+      ['search', 'exit'],
+    );
+    if (selection === 'exit') {
+      process.exit(0);
+    }
+    const line = await input.text(
+      `${selection[0].toUpperCase()}${selection.substr(1)} :`,
+    );
+    switch (selection) {
+      case 'search':
+        getKey(line).then((result) => {
+          if (result.length < 1) {
+            console.log('Not found!');
+          } else {
+            console.log(result);
+          }
+        });
+        break;
+      default:
+        console.log('Unknown Command!');
+        break;
+    }
+    await new Promise((r) => setTimeout(r, 800));
+  }
+})();
