2640a902dca89f2bf040aeb76d376e7a27b6ed9a
Roxana Mafteiu-Scai <roxanamafteiuscai@gmail.com>
2021-09-30 15:09:29 +0200

    feat: add script to seed redis

---
+++ src/redis/seed.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+
+import { createReadStream }from 'fs';
+import { createInterface } from 'readline';
+import { once } from 'events';
+import path from 'path';
+import { setKey } from './client';
+
+(async () => {
+  try {
+    const targetPath = path.resolve(
+      __dirname,
+      '../../',
+      'data/wordlist.txt',
+    );
+
+    const rl = createInterface({
+      input: createReadStream(targetPath),
+      crlfDelay: Infinity
+    });
+
+    rl.on('line', (line) => setKey(line));
+
+    await once(rl, 'close');
+
+    console.log('File processed.');
+  } catch (err) {
+    console.error(err);
+  }
+
+ })().then(() => process.exit(0));
