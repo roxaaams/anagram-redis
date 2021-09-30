dfdb1ec31e92568b797269da792c2b5433099f57
Roxana Mafteiu-Scai <roxanamafteiuscai@gmail.com>
2021-09-30 15:08:36 +0200

    feat: add redis client. get and set redis after key

---
+++ src/redis/client.js
@@ -0,0 +1,23 @@
+import redis from 'redis';
+import { promisify } from 'util';
+import { sortString } from '../utils';
+
+const config = {
+  host: 'db',
+  port: '6379',
+};
+
+const client = redis.createClient(config);
+
+const rpush = promisify(client.rpush).bind(client);
+const lrange = promisify(client.lrange).bind(client);
+
+export const setKey = async (line) => {
+  const key = sortString(line);
+  await rpush(key, line);
+}
+
+export const getKey =  (line) => {
+  const key = sortString(line);
+  return lrange(key, 0, -1);
+}
